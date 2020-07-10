693874685b4c8b8e9163479ac8e691c05c755041
Jason <28811675+xynoipse@users.noreply.github.com>
2020-07-10 21:09:24 +0800

    fix: user role can't update profile

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
     public function __construct()
     {
         $this->middleware('admin:super')->only('destroy');
-        $this->middleware('admin')->except('index');
+        $this->middleware('admin')->except('index', 'updateProfile');
     }
 
     /**
