344938bde62b3c833fae19c950898b0dfc0efc41
Jason <28811675+xynoipse@users.noreply.github.com>
2020-07-29 22:31:35 +0800

    chore: update layout footer vue component

--- resources/js/layout/partials/footer/Footer.vue
+++ resources/js/layout/partials/footer/Footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="main-footer text-sm">
     <div class="float-right d-none d-sm-inline">
-      <b>xynoipsec</b>/lvdev
+      <b>xynoipse</b>/lvdev
     </div>
 
     <strong>
       Copyright &copy; 2020
-      <a href="//github.com/xynoipsec/lvdev" target="_blank">Lvdev</a>.
+      <a href="//github.com/xynoipse/lvdev" target="_blank">Lvdev</a>.
     </strong> All rights reserved.
   </footer>
 </template>
